26f04be31639e649e1f2bc5fdb19bcc2037fa5a7
Miguel Ângelo <miguelcg958@gmail.com>
2022-09-23 16:17:03 -0300

    feat: create default app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ () => null } />
+      <Route exact path="/meals" component={ () => null } />
+      <Route exact path="/drinks" component={ () => null } />
+      <Route exact path="/meals/:id" component={ () => null } />
+      <Route exact path="/drinks/:id" component={ () => null } />
+      <Route exact path="/meals/:id/in-progress" component={ () => null } />
+      <Route exact path="/drinks/:id/in-progress" component={ () => null } />
+      <Route exact path="/profile" component={ () => null } />
+      <Route exact path="/done-recipes" component={ () => null } />
+      <Route exact path="/favorite-recipes" component={ () => null } />
+    </Switch>
   );
 }
 
