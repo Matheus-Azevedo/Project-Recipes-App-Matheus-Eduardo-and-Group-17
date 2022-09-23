2b2b70dbc9e170edbea8cf60b5d90f2ba79c79e3
Emerson Florencio <emersonflorencio@hotmail.com>
2022-09-23 19:43:48 -0300

    Finalizando o Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,12 +11,20 @@ function Footer() {
       <Link
         to="/drinks"
       >
-        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="Icone de Bebidas" />
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Icone de Bebidas"
+        />
       </Link>
       <Link
         to="/meals"
       >
-        <img data-testid="meals-bottom-btn" src={ mealIcon } alt="Icone de Comidas" />
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt="Icone de Comidas"
+        />
       </Link>
     </footer>
   );
