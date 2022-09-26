14f0f6864e2c8abe905023fa4491a591f3c5f9c7
Matheus <matheuseduardo.jp@gmail.com>
2022-09-26 16:07:58 -0300

    Alterações Requisito 8

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import Header from '../components/Header';
 
 function Profile() {
-  const TITLE_PAGE = 'Profile';
   return (
     <div>
-      <Header title={ TITLE_PAGE } />
+      <Header />
     </div>
   );
 }
