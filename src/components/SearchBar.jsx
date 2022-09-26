d5bfd192e025cad3d7c9072bd4c19720a9f809e1
Matheus <matheuseduardo.jp@gmail.com>
2022-09-26 20:56:42 -0300

    Testes 08 e 09 fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,15 +4,30 @@ function SearchBar() {
   return (
     <fieldset>
       <label htmlFor="name-search-radio">
-        <input data-testid="name-search-radio" type="radio" />
+        <input
+          data-testid="name-search-radio"
+          type="radio"
+          name="search-radio"
+          value="Ingrediente"
+        />
         Ingrediente
       </label>
       <label htmlFor="ingredient-search-radio">
-        <input data-testid="ingredient-search-radio" type="radio" />
+        <input
+          data-testid="ingredient-search-radio"
+          type="radio"
+          name="search-radio"
+          value="Nome"
+        />
         Nome
       </label>
       <label htmlFor="first-letter-search-radio">
-        <input data-testid="first-letter-search-radio" type="radio" />
+        <input
+          data-testid="first-letter-search-radio"
+          type="radio"
+          name="search-radio"
+          value="primeira-letra"
+        />
         Primeira Letra
       </label>
       <label htmlFor="search-input">
@@ -29,3 +44,7 @@ function SearchBar() {
 }
 
 export default SearchBar;
+
+// https://www.themealdb.com/api/json/v1/1/filter.php?i={ingrediente};
+// https://www.themealdb.com/api/json/v1/1/search.php?s={nome};
+// https://www.themealdb.com/api/json/v1/1/search.php?f={primeira-letra};
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,40 +1,59 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-import DoneRecipes from '../pages/DoneRecipes';
-import Recipes from '../pages/Recipes';
+import App from '../App';
+import mockFetch from '../../cypress/mocks/meals';
 
 describe('Testando o componente Header através da página <Recipes />', () => {
-  test('Verifica se existe o Titulo Meals', () => {
-    renderWithRouter(<Recipes />);
+  afterEach(() => { global.fetch.mockClear(); });
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockFetch),
+    });
+  });
+
+  test('Verifica se existe o Titulo Meals', async () => {
+    renderWithRouter(<App />, {}, '/meals');
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
     const pageTitle = screen.getByText(/Meals/i);
     expect(pageTitle.innerHTML).toEqual('Meals');
   });
   test('Verifica se ao clicar no btn Profile o usuário é redirecionado', () => {
-    const { history } = renderWithRouter(<Recipes />);
+    const { history } = renderWithRouter(<App />, {}, '/profile');
     const profileBtn = screen.getByTestId('profile-top-btn');
     userEvent.click(profileBtn);
     const { location: { pathname } } = history;
     expect(pathname).toEqual('/profile');
   });
-  test('Verifica se ao clicar no btn Search a barra é mostrada', () => {
-    renderWithRouter(<Recipes />);
+  test('Verifica se ao clicar no btn Search a barra é mostrada', async () => {
+    renderWithRouter(<App />, {}, '/meals');
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const searchInput = screen.getByTestId('search-input');
     expect(searchInput).toBeInTheDocument();
   });
-  test('Verifica se ao clicar no btn Search pela segunda vez a barra some', () => {
-    renderWithRouter(<Recipes />);
+  test('Verifica se ao clicar no btn Search pela segunda vez a barra some', async () => {
+    renderWithRouter(<App />, {}, '/meals');
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const searchInput = screen.getByTestId('search-input');
     userEvent.click(searchBtn);
     expect(searchInput).not.toBeInTheDocument();
   });
-  test('Verifica se existe apenas o button para o Profile na pagina Done Recipes', () => {
-    renderWithRouter(<DoneRecipes />);
+  test('Verifica se existe apenas o button para o Profile na pagina Done Recipes', async () => {
+    renderWithRouter(<App />, {}, '/done-recipes');
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
+    });
     const profileBtn = screen.getByTestId('profile-top-btn');
     expect(profileBtn).toBeInTheDocument();
   });
