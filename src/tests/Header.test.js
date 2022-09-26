33dbb372d67a75a9e6989a98364b041bb8a498cb
Matheus <matheuseduardo.jp@gmail.com>
2022-09-26 16:59:38 -0300

    Requisito 8

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import renderWithRouter from './renderWithRouter';
 import DoneRecipes from '../pages/DoneRecipes';
 import Recipes from '../pages/Recipes';
 
 describe('Testando o componente Header através da página <Recipes />', () => {
   test('Verifica se existe o Titulo Meals', () => {
-    renderWithRouterAndRedux(<Recipes />);
+    renderWithRouter(<Recipes />);
     const pageTitle = screen.getByText(/Meals/i);
     expect(pageTitle.innerHTML).toEqual('Meals');
   });
   test('Verifica se ao clicar no btn Profile o usuário é redirecionado', () => {
-    const { history } = renderWithRouterAndRedux(<Recipes />);
+    const { history } = renderWithRouter(<Recipes />);
     const profileBtn = screen.getByTestId('profile-top-btn');
     userEvent.click(profileBtn);
     const { location: { pathname } } = history;
     expect(pathname).toEqual('/profile');
   });
   test('Verifica se ao clicar no btn Search a barra é mostrada', () => {
-    renderWithRouterAndRedux(<Recipes />);
+    renderWithRouter(<Recipes />);
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const searchInput = screen.getByTestId('search-input');
     expect(searchInput).toBeInTheDocument();
   });
   test('Verifica se ao clicar no btn Search pela segunda vez a barra some', () => {
-    renderWithRouterAndRedux(<Recipes />);
+    renderWithRouter(<Recipes />);
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const searchInput = screen.getByTestId('search-input');
@@ -34,7 +34,7 @@ describe('Testando o componente Header através da página <Recipes />', () => {
     expect(searchInput).not.toBeInTheDocument();
   });
   test('Verifica se existe apenas o button para o Profile na pagina Done Recipes', () => {
-    renderWithRouterAndRedux(<DoneRecipes />);
+    renderWithRouter(<DoneRecipes />);
     const profileBtn = screen.getByTestId('profile-top-btn');
     expect(profileBtn).toBeInTheDocument();
   });
