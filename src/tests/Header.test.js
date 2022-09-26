27338ecc95ea0c713497269d6ae88a1478f28287
Matheus <matheuseduardo.jp@gmail.com>
2022-09-26 15:41:46 -0300

    Requisito 8

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Meals from '../pages/Meals';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import DoneRecipes from '../pages/DoneRecipes';
+import Recipes from '../pages/Recipes';
 
-describe('Testando o componente Header através da página <Meals />', () => {
+describe('Testando o componente Header através da página <Recipes />', () => {
   test('Verifica se existe o Titulo Meals', () => {
-    renderWithRouterAndRedux(<Meals />);
+    renderWithRouterAndRedux(<Recipes />);
     const pageTitle = screen.getByText(/Meals/i);
     expect(pageTitle.innerHTML).toEqual('Meals');
   });
   test('Verifica se ao clicar no btn Profile o usuário é redirecionado', () => {
-    const { history } = renderWithRouterAndRedux(<Meals />);
+    const { history } = renderWithRouterAndRedux(<Recipes />);
     const profileBtn = screen.getByTestId('profile-top-btn');
     userEvent.click(profileBtn);
     const { location: { pathname } } = history;
     expect(pathname).toEqual('/profile');
   });
   test('Verifica se ao clicar no btn Search a barra é mostrada', () => {
-    renderWithRouterAndRedux(<Meals />);
+    renderWithRouterAndRedux(<Recipes />);
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const searchInput = screen.getByTestId('search-input');
     expect(searchInput).toBeInTheDocument();
   });
   test('Verifica se ao clicar no btn Search pela segunda vez a barra some', () => {
-    renderWithRouterAndRedux(<Meals />);
+    renderWithRouterAndRedux(<Recipes />);
     const searchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(searchBtn);
     const searchInput = screen.getByTestId('search-input');
