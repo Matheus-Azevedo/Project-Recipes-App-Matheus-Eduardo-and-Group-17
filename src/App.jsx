8911d0c3793905d19549667d7420a19f324797be
Rafael Miranda <35074501+rafamirandaa@users.noreply.github.com>
2022-09-27 15:50:59 -0300

    Requisito 37

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FavoriteRecipes from './pages/FavoriteRecipes';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import RecipeDetails from './pages/RecipeDetails';
+import RecipeInProgress from './pages/RecipeInProgress';
 import Recipes from './pages/Recipes';
 
 function App() {
@@ -17,8 +18,8 @@ function App() {
       <Route exact path="/drinks" component={ Recipes } />
       <Route exact path="/meals/:id" component={ RecipeDetails } />
       <Route exact path="/drinks/:id" component={ RecipeDetails } />
-      <Route exact path="/meals/:id/in-progress" component={ () => null } />
-      <Route exact path="/drinks/:id/in-progress" component={ () => null } />
+      <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
+      <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
