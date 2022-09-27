fdedc6ba775b3be3be9b336220e6dcd8993be0b2
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-09-27 16:51:41 -0300

    requisito 58

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,12 +7,12 @@ function Profile() {
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem('user')));
   }, []);
-
+  const { email } = user;
   return (
     <div>
       <Header />
       <section>
-        <p data-testid="profile-email">{user.email}</p>
+        <p data-testid="profile-email">{email}</p>
         <button type="button" data-testid="profile-done-btn">Done Recipes</button>
         <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button type="button" data-testid="profile-logout-btn">Logout</button>
