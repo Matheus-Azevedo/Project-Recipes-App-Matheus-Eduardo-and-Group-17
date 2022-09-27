5736353281914db8c0de9ff8d95a5e883d5455b1
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-09-27 17:04:20 -0300

    requisito 60

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -8,12 +9,20 @@ function Profile() {
     setUser(JSON.parse(localStorage.getItem('user')));
   }, []);
   const { email } = user;
+  const history = useHistory();
   return (
     <div>
       <Header />
       <section>
         <p data-testid="profile-email">{email}</p>
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+
+        </button>
         <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button type="button" data-testid="profile-logout-btn">Logout</button>
       </section>
