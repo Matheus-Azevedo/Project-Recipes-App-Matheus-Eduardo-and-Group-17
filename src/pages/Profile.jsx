4d3c6847345befedff219ab0139fc5a9a23809e0
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-09-27 16:49:38 -0300

    requisito 58

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user')));
+  }, []);
+
   return (
     <div>
       <Header />
       <section>
-        <p data-testid="profile-email">Email</p>
+        <p data-testid="profile-email">{user.email}</p>
         <button type="button" data-testid="profile-done-btn">Done Recipes</button>
         <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button type="button" data-testid="profile-logout-btn">Logout</button>
